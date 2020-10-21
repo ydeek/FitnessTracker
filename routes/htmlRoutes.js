987114a6129e82d3355ec3eb0ea98305d7d2af2a
Yazan Deek <ydeek91@yahoo.com>
2020-10-21 18:08:14 -0400

    modified htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,3 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
 var path = require("path");
 
 module.exports = function (app) {
